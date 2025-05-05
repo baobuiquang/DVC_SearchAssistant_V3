3da2c272487f9be9d4e959142119504b77e3869c
Bao Bui-Quang <quangbao.dev@gmail.com>
2025-05-05 11:29:55 +0700

    Update DATA_HARDCODE.py

--- static/DATA_HARDCODE.py
+++ static/DATA_HARDCODE.py
@@ -403,4 +403,201 @@ Tra cứu hồ sơ trực tuyến tại: https://dichvucong.langson.gov.vn/dichv
     # ====================================================================================================
     # ====================================================================================================
     # ====================================================================================================
+    "DVC_TTHC_BoCongAn": [
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        {
+            "questions": [
+                "hướng dẫn nộp hồ sơ",
+                "hướng dẫn làm thủ tục",
+                "hướng dẫn sử dụng",
+                "hướng dẫn chung",
+                "làm thủ tục", "nộp thủ tục",
+                "làm hồ sơ", "nộp hồ sơ",
+                "hướng dẫn",
+            ],
+            "answer": {
+                "input": "[👋]",
+                "datapool": "DVC_TTHC_BoCongAn",
+                "name": "Hướng dẫn sử dụng cổng dịch vụ công để nộp hồ sơ/thủ tục",
+                "link": "",
+                "code": "",
+                "content_0": """\
+<h2>Hướng dẫn sử dụng cổng dịch vụ công để nộp hồ sơ/thủ tục</h2>
+<h3>Bước 1: Đăng nhập</h3>
+<ul>
+<li>Truy cập trang chủ Dịch vụ công Bộ Công An: https://dichvucong.bocongan.gov.vn</li>
+<li>Bấm nút [Đăng nhập] -> Chọn [Tài khoản Định danh điện tử cấp bởi Bộ Công an dành cho Công dân]</li>
+<li>Đăng nhập bằng mã định danh/mật khẩu hoặc mở app VNeID để quét QR code.</li>
+</ul>
+<h3>Bước 2: Tiến hành nộp hồ sơ</h3>
+<ul>
+<li>Vào Menu [Dịch vụ công trực tuyến], tìm thủ tục theo Mã thủ tục hoặc tên thủ tục, bấm nút [Tìm kiếm]</li>
+<li>Bấm nút [Nộp trực tuyến] để bắt đầu nộp hồ sơ</li>
+<li>Chọn Cấp thủ tục, đơn vị cần nộp hồ sơ -> Bấm [Đồng ý]</li>
+<li>Điền đầy đủ thông tin người nộp -> Bấm [Tiếp tục]</li>
+<li>Đính kèm thành phần hồ sơ -> Bấm [Tiếp tục]</li>
+<li>Nhập đủ thông tin -> Chọn ngân hàng thanh toán -> Bấm [Thanh toán]</li>
+</ul>
+<p>Sau khi thanh toán, bạn sẽ được chuyển về màn hình nộp hồ sơ thành công.</p>
+<h3>Xem đầy đủ văn bản hướng dẫn tại:</h3>
+<a href='https://dichvucong.bocongan.gov.vn/dichvucong/congdan/huongdancongdan' target='_blank'>https://dichvucong.bocongan.gov.vn/dichvucong/congdan/huongdancongdan</a>
+""",
+                "content_1": "DO-NOT-USE-THIS",
+                "content_2": "DO-NOT-USE-THIS",
+                "content_data": {
+                    "raw": """\
+Bước 1: Đăng nhập
+* Truy cập trang chủ Dịch vụ công Bộ Công An: https://dichvucong.bocongan.gov.vn
+* Bấm nút [Đăng nhập] -> Chọn [Tài khoản Định danh điện tử cấp bởi Bộ Công an dành cho Công dân]
+* Đăng nhập bằng mã định danh/mật khẩu hoặc mở app VNeID để quét QR code.
+Bước 2: Tiến hành nộp hồ sơ
+* Vào Menu [Dịch vụ công trực tuyến], tìm thủ tục theo Mã thủ tục hoặc tên thủ tục, bấm nút [Tìm kiếm]
+* Bấm nút [Nộp trực tuyến] để bắt đầu nộp hồ sơ
+* Chọn Cấp thủ tục, đơn vị cần nộp hồ sơ -> Bấm [Đồng ý]
+* Điền đầy đủ thông tin người nộp -> Bấm [Tiếp tục]
+* Đính kèm thành phần hồ sơ -> Bấm [Tiếp tục]
+Ghi chú: Nếu thành phần hồ sơ nào không có nộp thì bỏ dấu tích ở đầu
+* Nhập đủ thông tin -> Chọn ngân hàng thanh toán -> Bấm [Thanh toán]
+Sau khi thanh toán, bạn sẽ được chuyển về màn hình nộp hồ sơ thành công.
+"""
+                },
+                "suggestions": [],
+            }
+        },
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        {
+            "questions": [
+                "đường dây nóng", "hotline", "số điện thoại"
+            ],
+            "answer": {
+                "input": "[👋]",
+                "datapool": "DVC_TTHC_BoCongAn",
+                "name": "Đường dây nóng hỗ trợ",
+                "link": "",
+                "code": "",
+                "content_0": """\
+<h2>Đường dây nóng hỗ trợ</h2>
+<h3>Thông tin đường dây nóng:</h3>
+<p>Thông tin đường dây nóng: <a href='https://dichvucong.bocongan.gov.vn/bocongan/tracuu/tracuuhotline' target='_blank'>https://dichvucong.bocongan.gov.vn/bocongan/tracuu/tracuuhotline</a></p>
+""",
+                "content_1": "DO-NOT-USE-THIS",
+                "content_2": "DO-NOT-USE-THIS",
+                "content_data": {
+                    "raw": """\
+Thông tin đường dây nóng: https://dichvucong.bocongan.gov.vn/bocongan/tracuu/tracuuhotline
+"""
+                },
+                "suggestions": [],
+            }
+        },
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        {
+            "questions": [
+                "tra cứu hồ sơ", "hồ sơ trực tuyến", "tìm kiếm hồ sơ",
+                "tra cứu",
+            ],
+            "answer": {
+                "input": "[👋]",
+                "datapool": "DVC_TTHC_BoCongAn",
+                "name": "Tra cứu hồ sơ trực tuyến",
+                "link": "",
+                "code": "",
+                "content_0": """\
+<h2>Tra cứu hồ sơ trực tuyến</h2>
+<p>
+Tra cứu hồ sơ trực tuyến tại: <a href='https://dichvucong.bocongan.gov.vn/bocongan/tracuu' target='_blank'>https://dichvucong.bocongan.gov.vn/bocongan/tracuu</a>
+</p>
+""",
+                "content_1": "DO-NOT-USE-THIS",
+                "content_2": "DO-NOT-USE-THIS",
+                "content_data": {
+                    "raw": """\
+Tra cứu hồ sơ trực tuyến tại: https://dichvucong.bocongan.gov.vn/bocongan/tracuu
+"""
+                },
+                "suggestions": [],
+            }
+        },
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        {
+            "questions": [
+            ],
+            "answer": {
+                "input": "[👋]",
+                "datapool": "DVC_TTHC_BoCongAn",
+                "name": "🍌🍌🍌🍌🍌🍌🍌🍌🍌🍌",
+                "link": "",
+                "code": "",
+                "content_0": """\
+""",
+                "content_1": "DO-NOT-USE-THIS",
+                "content_2": "DO-NOT-USE-THIS",
+                "content_data": {
+                    "raw": """\
+"""
+                },
+                "suggestions": [],
+            }
+        },
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        {
+            "questions": [
+            ],
+            "answer": {
+                "input": "[👋]",
+                "datapool": "DVC_TTHC_BoCongAn",
+                "name": "🍌🍌🍌🍌🍌🍌🍌🍌🍌🍌",
+                "link": "",
+                "code": "",
+                "content_0": """\
+""",
+                "content_1": "DO-NOT-USE-THIS",
+                "content_2": "DO-NOT-USE-THIS",
+                "content_data": {
+                    "raw": """\
+"""
+                },
+                "suggestions": [],
+            }
+        },
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        {
+            "questions": [
+            ],
+            "answer": {
+                "input": "[👋]",
+                "datapool": "DVC_TTHC_BoCongAn",
+                "name": "🍌🍌🍌🍌🍌🍌🍌🍌🍌🍌",
+                "link": "",
+                "code": "",
+                "content_0": """\
+""",
+                "content_1": "DO-NOT-USE-THIS",
+                "content_2": "DO-NOT-USE-THIS",
+                "content_data": {
+                    "raw": """\
+"""
+                },
+                "suggestions": [],
+            }
+        },
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------
+    ],
+    # ====================================================================================================
+    # ====================================================================================================
+    # ====================================================================================================
 }
